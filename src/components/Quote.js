8ef28c586fa7e1467bfa396eef7c8ced45b05d3c
Eddie Haddican <edwardhaddican@gmail.com>
2021-05-11 22:26:38 -0400

    started the quote component

---
+++ src/components/Quote.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+
+const Quote = (props) => {
+
+
+  return(
+    <div className="nav_bar_container">
+      <div className="link_container">
+        <h1>Quotes go here</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Quote
