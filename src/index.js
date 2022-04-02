9c2507b3d9fac111c282964f7980428c6b847e09
Eddie Haddican <edwardhaddican@gmail.com>
2022-04-02 11:52:56 -0400

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom'
-import {Main} from './components'
-import './styles/main.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Main } from "./components";
+import "./styles/main.css";
+// import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <Router>
-    <Main/>
+    <Main />
   </Router>,
-  document.getElementById('app')
+  document.getElementById("app")
 );
+
+// serviceWorker.register();
