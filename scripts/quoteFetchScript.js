20ae88e23a513de911dca1e637622b1d24041348
Karen MacPherson <karen@enveritas.org>
2022-06-01 14:40:46 -0400

    get all quotes and put in quotefile

--- scripts/quoteFetchScript.js
+++ scripts/quoteFetchScript.js
@@ -1,14 +1,13 @@
 const axios = require("axios");
+const fs = require('fs');
 
 const fetchAllQuotes = async () => {
   try {
     const response = await axios.get("https://type.fit/api/quotes");
-    const quoteArr = []
-    response.data.map((e)=>{
-      quoteArr.push(e.text)
-    })
-    console.log(quoteArr)
-    return response.data;
+
+    const quoteArr = response.data.map(e => e.text)
+    fs.writeFileSync('./quotes.json', JSON.stringify(quoteArr))
+
   } catch (err) {
     throw err;
   }
