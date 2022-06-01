7bf0ccd62c1f94ae3331d33a7b187d521c84381a
Karen MacPherson <karen@enveritas.org>
2022-06-01 14:10:12 -0400

    move sw into src to match cra's expectations

--- src/index.js
+++ src/index.js
@@ -12,4 +12,12 @@ ReactDOM.render(
   document.getElementById("app")
 );
 
+if (!navigator.serviceWorker.controller) {
+  navigator.serviceWorker.register("/sw.js").then(function (reg) {
+    console.log(
+      "Service worker has been registered for scope: " + reg.scope
+    );
+  });
+}
+
 // serviceWorker.register();
